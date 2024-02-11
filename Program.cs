dc3117d5f9c18b206582f02aeb3d1362f306c939
LesFerch <79026235+LesFerch@users.noreply.github.com>
2024-02-11 17:55:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using UtfUnknown;
+
+namespace ConvertToUTF8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Convert files to UTF-8");
+                Console.WriteLine("Usage: ConvertToUTF8 <file1> <file2> ...");
+                return;
+            }
+
+            Console.WriteLine("sep=;"); // For Excel
+
+            foreach (string path in args)
+            {
+                try
+                {
+                    Console.Write($"\"{path}\";");
+
+                    // Detect encoding using UtfUnknown
+                    Encoding detectedEncoding = DetectEncoding(path);
+
+                    // Read the file using detected encoding and convert to UTF-8
+                    bool BOM = false;
+                    string readText = File.ReadAllText(path, detectedEncoding);
+                    File.WriteAllText(path, readText, new UTF8Encoding(BOM));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error converting file {path}: {ex.Message}");
+                }
+            }
+        }
+
+        static Encoding DetectEncoding(string filePath)
+        {
+            try
+            {
+                DetectionResult result = CharsetDetector.DetectFromFile(filePath);
+                DetectionDetail resultDetected = result.Detected;
+                string encodingName = resultDetected.EncodingName;
+                Encoding encoding = resultDetected.Encoding;
+                Console.WriteLine(encodingName);
+                return encoding;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error detecting encoding for file {filePath}: {ex.Message}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ConvertToUTF8
-Convert text file to UTF-8 with encoding auto-detection
+
+Command line tool to convert one or more text files from other encodings (e.g. Shift JIS) to UTF-8.
+
+This was written primarily for converting non-UTF-8 Japanese, Arabic, etc. SRT files, but should work fine on any text file.
+
+Note that there is no harm converting a file that's already UTF-8. All that will happen is any UTF-8 with BOM will be changed to UTF-8 without BOM.
+
+This program uses the [UTF Unknown](https://github.com/CharsetDetector/UTF-unknown) library to detect the source file encoding (aka code page).
+
+**WARNING**: This program overwrites the original file with the converted file! You may want to make a backup copy first!
+
+[![image](https://user-images.githubusercontent.com/79026235/152910441-59ba653c-5607-4f59-90c0-bc2851bf2688.png)Download the zip file](https://github.com/LesFerch/ConvertToUTF8/releases/download/1.0.0/ConvertToUTF8.zip)
+
+## How to Download and Run
+
+1. Download the zip file using the link above.
+2. Extract **ConvertToUTF8.exe** and **UtfUnknown.dll**.
+3. Right-click **ConvertToUTF8.exe**, select Properties, check Unblock, and click OK.
+4. Optionally move **ConvertToUTF8.exe** and **UtfUnknown.dll** to the folder of your choice.
+5. Drag and drop text files onto **ConvertToUTF8.exe** to convert them to UTF-8. Or run it from the command line.
+6. If you skipped step 3, then, in the SmartScreen window, click More info and then Run anyway.
+
+**Note**: Some antivirus software may falsely detect the download as a virus. This can happen any time you download a new executable and may require extra steps to whitelist the file.
+
+[![image](https://user-images.githubusercontent.com/79026235/153264696-8ec747dd-37ec-4fc1-89a1-3d6ea3259a95.png)](https://github.com/LesFerch/ConvertToUTF8)
